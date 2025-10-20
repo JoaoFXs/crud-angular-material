dcd838f7fdecf9d05c1e6a7bab57f59c66f62536
jovibfel@gmail.com <jovibfel@gmail.com>
2025-10-20 15:12:51 -0300

    Criando toolbar e configurando navegação entre páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { CadastroComponent } from './cadastro/cadastro.component'
+import { ConsultaComponent } from './consulta/consulta.component';
+//Classe com constant para cadastro de rotas e seus components
+export const routes: Routes = [
+    {
+        path: 'cadastro', component: CadastroComponent//Caminho + Component
+    },
+    {
+        path: 'consulta', component: ConsultaComponent
+    }
+];
